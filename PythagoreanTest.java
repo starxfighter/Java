147680c32c76cebd387bc2496dd4ac42d65b78db
Duane Osburn <dosburn@rocketmail.com>
2018-06-04 11:45:31 -0700

    Working on Java Fundamentals

---
+++ PythagoreanTest.java
@@ -0,0 +1,8 @@
+public class PythagoreanTest {
+    public static void main(String[] args) {
+        Pythagorean pt = new Pythagorean();
+        double result = pt.calculateHypotenuse(7, 8);
+        System.out.println("The value is: " + result);
+    }
+}
+
