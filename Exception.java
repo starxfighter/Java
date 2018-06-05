06f17ac7c0977edfba3115bc71f218c6e2ac3999
Duane Osburn <dosburn@rocketmail.com>
2018-06-04 17:35:45 -0700

    Exceptiopn Task Completed

---
+++ Exception.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+public class Exception {
+    public static void main(String[] args){
+        ArrayList<Object> myList = new ArrayList<Object>();
+        myList.add("13");
+        myList.add("hello world");
+        myList.add(48);
+        myList.add("Goodbye World");
+        for(int i = 0; i < myList.size(); i++){
+            try{
+                Integer castedValue = (Integer) myList.get(i);
+                System.out.println("Casting worked");    
+            } catch (ClassCastException e){
+                System.out.println("Error. Casting failed");
+                System.out.println("Bad Value: " + myList.get(i));
+                System.out.println("The bad index: " + i);
+            }
+        }
+    }
+}
